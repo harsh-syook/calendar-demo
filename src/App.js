e183e9ceefd5bca54c91ebe9d05464dc7c267d7f
harsh <harsh@localhost.localdomain>
2019-11-26 14:36:23 +0530

    react-big-calendar demo test

--- src/App.js
+++ src/App.js
@@ -1,48 +1,95 @@
 import React from 'react';
 import {Calendar, momentLocalizer} from 'react-big-calendar'
+import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop'
 import moment from 'moment'
+
+
 import './App.css';
 import 'react-big-calendar/lib/css/react-big-calendar.css'
+import 'react-big-calendar/lib/addons/dragAndDrop/styles.scss'
+
 moment.locale('en')
 const localizer = momentLocalizer(moment)
-const myEventsList=[
-  {
-    title: "test",
-    start: new Date(2019,10,24),
-    end: new Date(2019,10,24),
-    allDay: true
-  },
-  {
-    title: "test2",
-    start: new Date(2019,10,25),
-    end: new Date(2019,10,25),
-    allDay: true
-  },
-  {
-    title: "test3",
-    start: new Date(2019,10,25),
-    end: new Date(2019,10,25),
-    allDay: true
-  }
-]
+const DragAndDropCalendar = withDragAndDrop(Calendar)
 
-function App(props) {
-  return (
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={
+      events:[{
+        id:0,
+        title: "test",
+        start: new Date(2019,10,24),
+        end: new Date(2019,10,24),
+        allDay: true,
+        tooltip:"description goes here for test"
+      },
+      {
+        id:1,
+        title: "test2",
+        start: new Date(2019,10,25),
+        end: new Date(2019,10,25),
+        allDay: true,
+        tooltip:"description goes here for test2"
+      },
+      {
+        id:2,
+        title: "test3",
+        start: new Date(2019,10,25),
+        end: new Date(2019,10,25),
+        allDay: true,
+        tooltip:"description goes here for test3"
+      }]
+    }
+  }
+    handleSelect = ({ start, end }) => {
+      
+    const title = window.prompt('New Event name')
+    if(title){
+      this.setState(prev=>{
+        return{
+          events:[...prev.events,{title,start,end}]
+        }
+      })
+    }
+  }
+  moveEvent=({ event, start, end, isAllDay: droppedOnAllDaySlot })=>{
+    const { events } = this.state
+
+    const idx = events.indexOf(event)
+    let allDay = event.allDay
+
+    const updatedEvent = { ...event, start, end, allDay }
+
+    const nextEvents = [...events]
+    nextEvents.splice(idx, 1, updatedEvent)
+
+    this.setState({
+      events: nextEvents,
+    })
+
+    // alert(`${event.title} was dropped onto ${updatedEvent.start}`)
+  }
+  render(){
+    return (
     <div className="App">
-    <Calendar 
+    <DragAndDropCalendar 
+    selectable
     localizer={localizer}
-    hideGutter
-    hideTimeIndicator
-    step={60}
-    showMultiDayTimes
-    events={myEventsList}
-    getNow={()=>new Date()}
-    views={['week','month']}
+    tooltipAccessor={(event)=>event.tooltip}
+    events={this.state.events}
+    getNow={()=> new Date()}
+    onEventDrop={this.moveEvent}
+    views={['day','week','month']}
+    onSelectEvent={event => alert(event.title)}
+    onSelectSlot={this.handleSelect}
     
     />
+    
      
     </div>
-  );
+  )
+  }
 }
 
 export default App;
